b4b33dad6c8956860e4e1a975bcdefabc2e03cf7
Sacura <Sacuramec1@gmail.com>
2025-06-15 22:17:26 +0100

    final fix tonight

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/home';
 import PostDetail from './components/posts';
 import Footer from './components/footer';
